0fc7c341570a17b088734c64e592c4dd9a29b95d
DatSpirit <phanthanhdattin@gmail.com>
2025-11-24 15:30:30 +0700

    (*) D: D8 — User Migrations - Two Factor Auth & D9 — User Seeders & Factories & D10 — User Tests

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User; 
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        // 1. TÀI KHOẢN ADMIN (Để kiểm thử)
+        User::create([
+            'name' => 'Admin Tester',
+            'email' => 'admin@test.com', // Email dùng để đăng nhập
+            'password' => Hash::make('password'), // Mật khẩu là 'password'
+            'is_admin' => true, // Đánh dấu là Admin
+        ]);
+
+          // 1. TÀI KHOẢN ADMIN (Để kiểm thử)
+        User::create([
+            'name' => 'Thanh Dat',
+            'email' => 'datpt@gmail.com', // Email dùng để đăng nhập
+            'password' => Hash::make('12345678'), // Mật khẩu 
+            'is_admin' => true, // Đánh dấu là Admin
+        ]);
+
+
+
+         // Tạo user ngẫu nhiên : php artisan db:seed
+        User::factory()->count(100)->create();
+    }
+}
